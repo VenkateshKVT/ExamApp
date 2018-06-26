bd9e253f1771ec76eaf082d42fd4a69fa4ee4180
Venkatesh Kallepalli <32375733+VenkateshKVT@users.noreply.github.com>
2018-06-26 07:23:25 +0530

    Add files via upload

---
+++ ExamApp/js/app.js
@@ -0,0 +1,12 @@
+(function(){
+
+    /*
+     * Declaration of main angular module for this appllication.
+     *
+     * It is named turtleFacts and has no dependencies (hence the 
+     * empty array as the second argument)
+     */
+    angular
+        .module("turtleFacts", []);
+
+})();
